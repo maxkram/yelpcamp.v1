3d4e05828e5d98b3cc80193e2392bfe4eccc0e28
maxkramer <max.kramer@live.com>
2017-02-28 17:36:10 +0000

    with landing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
-
+app.set("view engine", "ejs");
 app.get("/", function(req, res){
-   res.send("Это будет лэндин пэйдж");
+   res.render("landing");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
